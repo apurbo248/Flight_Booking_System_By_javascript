4d39bf79d95b5789d5cb5d064e0c8b8e4f4d3b72
apurbo248 <apurbopal248@gmail.com>
2021-01-30 14:15:16 +0600

    Completed

---
+++ fly.js
@@ -0,0 +1,50 @@
+function flight(inputId,isIncrease,classPrice){
+    const inputCount = getInput(inputId);
+    let updateCount = inputCount ;
+   
+    if(isIncrease == true){
+        updateCount +=1;
+    }
+    if(isIncrease == false && inputCount>0){
+        updateCount -=1;
+    }
+    document.getElementById(inputId).value = updateCount;
+
+
+    let totalprice = 0;
+    if(classPrice == 'firstClassPrice'){
+        totalprice = updateCount * 150;
+    }
+    if(classPrice == 'economiyClassprice'){
+        totalprice = updateCount * 100
+    }
+
+    const subTotal1 = getInput('inputBox1');
+    const subTotal2 = getInput('inputBox2');
+    const subTotal = subTotal1 * 150 + subTotal2 * 100;
+    document.getElementById('subtotal').innerText = subTotal;
+
+    const vat = subTotal * 0.1;
+    document.getElementById('vat').innerText =vat;
+
+    const Total = subTotal + vat;
+    document.getElementById('total').innerText =Total;
+
+}
+
+function getInput(inputId){
+    const input = document.getElementById(inputId);
+    const inputType = parseInt(input.value);
+    return inputType;
+}
+
+function BookNow(){
+    const input1 = getInput('inputBox1');
+    const input2 = getInput('inputBox2');
+    const total = document.getElementById('total').innerText;
+    let t=input1+input2;
+    alert('********************Booked Flight Successfully********************* '+ 
+    '   Your total sit is -> '+t + '                             Total Price is -> ' + total+   "                       Wish you have a safe journey..."
+       
+    );
+}
